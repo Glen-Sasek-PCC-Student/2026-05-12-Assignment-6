dd08f7eb6cefb88c7453bb5bfb56b3a0e9275feb
Glen-Sasek-PCC-Student <glen.sasek+student@pcc.edu>
2026-05-13 02:31:52 +0000

    Partial implementation

--- main.cpp
+++ main.cpp
@@ -33,6 +33,7 @@
 
 // ------------- CODE -------------
 #include <iostream>
+#include <limits>
 
 using namespace std;
 
@@ -42,7 +43,91 @@ using namespace std;
 // Main function
 // https://en.cppreference.com/w/cpp/language/main_function.html
 int main(int argc, char* argv[]) {
-  cout << "Hello, World!" << endl;
+    cout << "Welcome to my Coffee/Tea Vending Machine!" << endl;
+
+    bool next_main_loop = true;
+    double balance_USD = 0.0;
+    while(next_main_loop) {
+        // Get Coins
+        bool next_coin = true;
+        while(next_coin) {
+            bool invalid_input = false;
+            int coin = -1;
+            cout << "Enter coins - 5, 10, or 25 only [0 to end input]: ";
+            cin >> coin;
+            if(cin) {
+                switch(coin) {
+                    case 0: 
+                        next_coin = false;
+                        break;
+                    case 5: // Fallthrough
+                    case 10: // Fallthrough
+                    case 25: // OK
+                        balance_USD += coin / 100.0;
+                        break;
+                    default:
+                        invalid_input = true;
+                }
+            } else {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+                invalid_input = true;
+            }
+            if(invalid_input) {
+                cout << "Bad input ;(" << endl;
+            }
+        }
+
+        // Get choice and count
+        bool next_option = true;
+        bool next_how_many = false;
+        char option = '\0';
+        while(next_option) {
+            cout << "Please pick an option ($0.25 each):" << endl;
+            cout << "    C/c: Coffee" << endl;
+            cout << "    T/t: Tea" << endl;
+            cout << "    Q/q: Quit" << endl;
+            
+            
+            cin >> option;
+            // Validate
+            next_option = false;
+            switch(option) {
+                case 'c':
+                    cout << "TODO: COFFEE!!" << endl;
+                    next_how_many = true;
+                    break;
+                case 't':
+                    cout << "TODO: TEA!!" << endl;
+                    next_how_many = true;
+                    break;
+                case 'q':
+                    next_main_loop = false;
+                    break;
+                default:
+                    cout << "Invalid Option! Please choose a valid option!" << endl;
+                    next_option = true;
+            }
+        }
+
+        int how_many = 0;
+        while(next_how_many) {
+            cout << "How many would you like?" << endl;
+            cin >> how_many;
+            if(cin) {
+                // process sale, check balance, etc...
+                cout << "TODO: Check cost and balance" << endl;
+                cout << "TODO: Make sale or get more coins" << endl;
+                next_main_loop = false;
+                next_how_many = false;
+            } else {
+                cin.clear();
+                cin.ignore(numeric_limits<streamsize>::max(), '\n');
+            }
+        }
+    }
+
+    cout << "Thank you for using my Vending Machine Program!" << endl;
   return 0;
 }
 
